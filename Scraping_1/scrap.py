de439c7409e0fc3584c131a248e9c005aba00dcf
Begravelsesnatt <8.No.Qu4rter@gmail.com>
2020-11-10 19:45:10 -0300

    Extrae links de url. Debiese soportar python 2 y 3.

---
+++ Scraping_1/scrap.py
@@ -0,0 +1,10 @@
+from six.moves import urllib
+from bs4 import BeautifulSoup
+
+datos = urllib.request.urlopen("https://www.youtube.com/playlist?list=PLBdkl5-ytBTwBjlnOh_rOcLy1NlFQ4Q4Q").read().decode('utf-8')
+
+soup =  BeautifulSoup(datos, 'html.parser')
+tags = soup("a")
+
+for tag in tags:
+	print(tag.get("href"))
