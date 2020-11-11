6081c69afcd143c3af5a44e5c9a0247a4291e32c
Begravelsesnatt <8.No.Qu4rter@gmail.com>
2020-11-11 02:12:51 -0300

    Cambio a 'lxml'

--- Scraping_1/scrap.py
+++ Scraping_1/scrap.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 datos = urllib.request.urlopen("https://www.youtube.com/playlist?list=PLBdkl5-ytBTwBjlnOh_rOcLy1NlFQ4Q4Q").read().decode('utf-8')
 
-soup =  BeautifulSoup(datos, 'html.parser')
+soup =  BeautifulSoup(datos, 'lxml')
 tags = soup("a")
 
 for tag in tags:
