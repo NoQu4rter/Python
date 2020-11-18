356a551c6dcf5b5356f4ab0d3fc378f2d3dd280b
Hail Murder <begravelsesnatt@kali.WORKGROUP>
2020-11-18 12:23:12 -0300

    Cambiamos de <a>, a extraer un re.Match object desde el contenido js de un tag <script>.\n Funciona para las versiones 2 y 3 de python.

--- Scraping_1/scrap.py
+++ Scraping_1/scrap.py
@@ -1,10 +1,32 @@
 from six.moves import urllib
 from bs4 import BeautifulSoup
+import json
+import re
 
 datos = urllib.request.urlopen("https://www.youtube.com/playlist?list=PLBdkl5-ytBTwBjlnOh_rOcLy1NlFQ4Q4Q").read().decode('utf-8')
 
-soup =  BeautifulSoup(datos, 'lxml')
-tags = soup("a")
+#k = 0
 
-for tag in tags:
-	print(tag.get("href"))
+soup = BeautifulSoup(datos, 'lxml')
+
+tag = soup.find_all('script')[26]
+
+data = re.search(".\"text\":\"11\".,.\"text\":\" videos\".", str(tag))
+
+print(data)
+
+#for tag in soup.find_all('script'):
+#
+#	k = k + 1
+#
+#	k = str(k)
+#
+#	print "[ETIQUETA " + k + "]"
+#	print (tag)
+#	k = int(k)
+
+#print ( soup.find(id="stats") )
+
+# "stats":[{"runs":[{"text":"11"},{"text":" videos"}]}
+# {"runs":[{"text":"11"},{"text":" videos"}]}
+# {"text":"11"},{"text":" videos"}
